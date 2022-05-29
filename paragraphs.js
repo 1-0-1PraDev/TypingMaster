18d0bb0f9111d355e87c464eb593ec95995e2e72
1-0-1PraDev <88771778+1-0-1PraDev@users.noreply.github.com>
2022-05-29 05:33:06 +0530

    Create paragraphs.js

---
+++ paragraphs.js
@@ -0,0 +1,55 @@
+const paragraphs = [
+    `Greenhouse effect is a natural process wherein sunlight passes through
+    atmosphere, warming earth’s surface. Then, the earth’s surface and water bodies
+    release energy in form of heat or infrared radiations into atmosphere and
+    maintains balance with the incoming energy. Greenhouse gases can absorb
+    some of the infrared radiation. But increase in greenhouse gases has led to more
+    absorption of infrared radiation which in turn has led to the increase of earth’s
+    surface temperature.`,
+
+    `Global warming is the latest alarm bell for earth’s environment. Global warming
+    refers to the increase in average temperature of earth’s surface during the last
+    century, from 1900-2000. Earth’s surface’s temperature has increased more than
+    one degree Fahrenheit since 1900. Global warming is alarming because of its
+    adverse effects on the ecological balance of earth. The resultant ecological
+    imbalance, due to global warming, is the root cause of various natural calamities
+    like floods, famines, etc. Also it is the root cause of various environmental threats
+    like increase in sea level, glacier melting, changes in quantity and pattern of
+    rainfall, heat waves, extreme seasons, etc. This is why global warming is the
+    buzzword of almost all environmental talks, projects and discussions, etc.`,
+
+    `There are so many great moments and speeches in this film. It really is a must see.
+    But for me, having gone to University and met people like Clark, I couldn’t help but 
+    fall in love with Will in this scene. This moment is just incredibly funny and
+    Damon’s delivery is spot-on. Will Hunting (Matt Damon) proves that you don’t have to
+    be book-smart to be intelligent, and that you shouldn’t look down on others just because
+    they don’t have the same fancy level of education that you do. This is the put-downs to end all put-downs"`,
+
+
+    `In the second instance, we hear the same speech again, but we are now in the bathroom of Brett’s apartment \
+    with one of Brett’s quaking friends. Hearing the speech in this way, and seeing this other guy’s
+     confused reaction to it, makes us re-evaluate what we have heard. Does it even make
+      sense? We were too busy being mesmerized by Jackson’s performance to actually think 
+      about what he was saying.`,
+
+      `What is important to remember is that this is not an anti-capitalist film.
+       It’s about two different types of capitalism: the ruthless kind advocated 
+       by Gekko and the more traditional kind practiced by his victims. So, what
+        Stone does here is very clever. At the shareholders’ meeting,
+       Gekko (a representative for all brokers) is given the chance to justify
+       his actions, and he does so by identifying the waste and slothfulness that
+       corporal America has acquired in the postwar years. 
+       He cleverly shifts the blame and is so eerily convincing that we’re 
+       almost inclined to agree with him; his audience certainly does.
+       To succeed in this way, this has to be a speech that is both well-written
+       and well-performed. Scarily, this speech is said to have inspired many young professionals to work on Wall Street too.`,
+
+      `It’s hard not to love Tom Hanks, but he really wins over his 
+      audience (and Reiben) with this speech. He teaches kids, for
+      goodness’ sake, and he’s leading a squad on a heroic mission.
+      And all he wants to do is go home to his wife.
+      Some might say that this pulls a little too much at the heart strings,
+      but I think it is beautiful. It has that David vs. Goliath-type quality to it,
+      where an ordinary man is about to rise up and do something truly heroic.
+      We love you Tom!`
+];
